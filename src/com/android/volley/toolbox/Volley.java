b62b57cada490583bbd9d3ee5e5f44945d93790a
Alex Davis <alex@welkinhealth.com>
2015-02-22 16:55:45 -0800

    Plumbing for custom SSL cert manager

--- src/com/android/volley/toolbox/Volley.java
+++ src/com/android/volley/toolbox/Volley.java
@@ -27,6 +27,8 @@ import com.android.volley.RequestQueue;
 
 import java.io.File;
 
+import javax.net.ssl.SSLSocketFactory;
+
 public class Volley {
 
     /** Default on-disk cache directory. */
@@ -39,7 +41,7 @@ public class Volley {
      * @param stack An {@link HttpStack} to use for the network, or null for default.
      * @return A started {@link RequestQueue} instance.
      */
-    public static RequestQueue newRequestQueue(Context context, HttpStack stack) {
+    public static RequestQueue newRequestQueue(Context context, HttpStack stack, SSLSocketFactory factory) {
         File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
 
         String userAgent = "volley/0";
@@ -52,7 +54,7 @@ public class Volley {
 
         if (stack == null) {
             if (Build.VERSION.SDK_INT >= 9) {
-                stack = new HurlStack();
+                stack = new HurlStack(null, factory);
             } else {
                 // Prior to Gingerbread, HttpUrlConnection was unreliable.
                 // See: http://android-developers.blogspot.com/2011/09/androids-http-clients.html
@@ -74,7 +76,7 @@ public class Volley {
      * @param context A {@link Context} to use for creating the cache dir.
      * @return A started {@link RequestQueue} instance.
      */
-    public static RequestQueue newRequestQueue(Context context) {
-        return newRequestQueue(context, null);
+    public static RequestQueue newRequestQueue(Context context, SSLSocketFactory factory) {
+        return newRequestQueue(context, null, factory);
     }
 }
